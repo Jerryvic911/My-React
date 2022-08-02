950347e2507b6e2ff5f7123eb98c1281ef6ef63e
jeremiah victor <Jeremiahv791@gmail.com>
2022-08-02 12:23:39 +0100

    my app

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Main(){
+    return(
+        <div className='main'>
+            <h1 className='main-text'>Fun Facts About React</h1>
+            <ul className='main-list'>
+                <li>was first relised in 2013</li>
+                <li>was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>powers thousands of enterprise apps,including mobile apps</li>
+            </ul>
+        </div>
+    )
+    
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar(){
+    return <nav>
+        <img src='../../logo192.png' alt=''/>
+        <h3>ReactsFacts</h3>
+        <h4>React course- project 1</h4>
+    </nav>
+    
+}
